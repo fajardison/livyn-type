dc034b18bc096c063ef1ddd3d5c702cb87b1308a
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2025-09-07 00:57:54 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import dataType from './type/dataType.js';
+import isType from './type/isType.js';
+import toType from './type/toType.js';
+import assertType from './type/assertType.js';
+
+export default {
+  dataType,
+  assertType,
+  isType,
+  toType
+}
+
+export {
+  dataType,
+  assertType,
+  isType,
+  toType
+}
